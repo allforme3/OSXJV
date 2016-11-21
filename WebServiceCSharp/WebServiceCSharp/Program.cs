7d1a57217daf68a8ff976d6edfbd84c43ebc293f
Allan Christopher Blundell <allforme3@msn.com>
2016-11-21 02:32:17 +0000

    Web Service
    First Building Blocks of the Web Service

---
+++ WebServiceCSharp/WebServiceCSharp/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+using System.IO;
+using System.Net;
+using Web;
+
+namespace WebServiceCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Httpserver s = new Httpserver();
+            s.Start();
+        }
+    }
+}
---
+++ WebServiceCSharp/WebServiceCSharp/Request.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Web
+{
+    public class Request
+    {
+        public string Value { get; set; }
+
+        public Request(string data)
+        {
+            Value = data;
+        }
+        public static Request GetRequest(string request)
+        {            
+            return new Request(request);
+        }
+    }
+}
